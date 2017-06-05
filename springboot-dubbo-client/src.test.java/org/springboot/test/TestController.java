7b5407a07d2e80bae7da1ae0b6f5284c1a52a438
huangleisir <huangleisir@qq.com>
2017-06-05 21:06:33 +0800

    加点单元测试

--- springboot-dubbo-client/src.test.java/org/springboot/test/TestController.java
+++ springboot-dubbo-client/src.test.java/org/springboot/test/TestController.java
@@ -9,15 +9,21 @@ import org.springframework.boot.test.context.TestComponent;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-//@RunWith(SpringJUnit4ClassRunner.class)  
+@RunWith(SpringJUnit4ClassRunner.class)  
 //这是Spring Boot注解，为了进行集成测试，需要通过这个注解加载和配置Spring应用上下  
 //@SpringBootConfiguration(classes = SpringBootApplication.class)  
-//@WebAppConfiguration  
+@WebAppConfiguration  
 public class TestController {
 	
 	@Test
 	public void test(){
 	new HelloWorldController().sayHi();
+	}
+	
+	
+	@Test
+	public void testSayHello(){
+	new HelloWorldController().sayHello();
 	}
 
 }
